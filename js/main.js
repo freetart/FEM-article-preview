fa246f73e1daf74b6b5e3cdb20393a50c3d2c8c0
habib <habibdev96@gmail.com>
2021-05-05 18:53:41 -0600

    created popup

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 "use strict";
+
+const popup = () => {
+  const shareBtn = document.querySelector(".share-btn");
+  const popupEl = document.querySelector(".popup");
+  const popupText = document.querySelector(".popuptext");
+
+  shareBtn.addEventListener("click", () => {
+    popupEl.classList.toggle("show");
+    popupText.classList.toggle("show");
+  });
+};
+popup();
